2f8401becbbce150b266b8997d780e2f30a9a12f
Vitor Stahelin <vitorjosestahelin@gmail.com>
2024-10-05 00:27:47 -0300

    feat: update paths

--- scripts/map_generator.py
+++ scripts/map_generator.py
@@ -4,13 +4,14 @@ from pathlib import Path
 
 
 def load_mapignore(root_dir):
-    mapignore_path = Path(root_dir) / '.mapignore'
+    mapignore_path = Path(root_dir) / ".mapignore"
     ignore_patterns = []
 
     if mapignore_path.is_file():
         with open(mapignore_path) as f:
-            ignore_patterns = [line.strip() for line in f if
-                               line.strip() and not line.startswith('#')]
+            ignore_patterns = [
+                line.strip() for line in f if line.strip() and not line.startswith("#")
+            ]
 
     return ignore_patterns
 
@@ -23,14 +24,21 @@ def should_ignore(path, ignore_patterns):
 
 
 def generate_json_structure(base_url, root_dir, ignore_patterns):
-    structure = {
-        "data": {},
-        "images": {},
-    }
+    structure = {}
 
     for dirpath, dirnames, filenames in os.walk(root_dir):
-        if '.git' in dirpath:
+        if ".git" in dirpath:
             continue
+
+        relative_dir = os.path.relpath(dirpath, root_dir)
+
+        if should_ignore(relative_dir, ignore_patterns):
+            continue
+
+        if relative_dir == ".":
+            current_structure = structure
+        else:
+            current_structure = structure.setdefault(relative_dir, {})
 
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
@@ -40,25 +48,23 @@ def generate_json_structure(base_url, root_dir, ignore_patterns):
                 continue
 
             url = f"{base_url}/{relative_path.replace(os.sep, '/')}"
-
-            if filename.endswith('.json'):
-                structure["data"][filename] = url
-            elif filename.endswith(('.png', '.jpg', '.jpeg', '.gif')):
-                structure["images"][filename] = url
-            else:
-                structure[filename] = url
+            current_structure[filename] = url
 
     return structure
 
 
 def main():
     base_url = "https://vstahelin.github.io/cms-portfolio"
-    root_dir = os.getcwd()
+    root_dir = Path(__file__).resolve().parent.parent
     ignore_patterns = load_mapignore(root_dir)
 
     json_structure = generate_json_structure(base_url, root_dir, ignore_patterns)
-    json_structure["site_map"] = f"{base_url}/site_map.json"
-    with open('site_map.json', 'w') as json_file:
+
+    if "." in json_structure:
+        json_structure.pop(".")
+
+    site_map_path = root_dir / "data" / "site_map.json"
+    with open(site_map_path, 'w') as json_file:
         json.dump(json_structure, json_file, indent=4)
 
 
